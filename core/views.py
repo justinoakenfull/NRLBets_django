ed2624af3dd3775e9c3cfa2bc87bd88e8f1cea7d
Justin Oakenfull <68644604+justinoakenfull@users.noreply.github.com>
2024-11-16 00:11:40 +1000

    Setup base styling with sidebar

--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 # Request Handler, not actually 'views' in the sense of 'views' in MVC
+
+def home(request):
+    return render(request, 'landing_page.html')
